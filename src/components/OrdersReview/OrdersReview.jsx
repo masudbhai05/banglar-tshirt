f2b73a8b231c859148eda7de530ed75069cd07d2
masudbhai05 <mdmasudali10102002@gmail.com>
2023-05-28 23:06:23 +0600

    this is first commit

---
+++ src/components/OrdersReview/OrdersReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrdersReview = () => {
+    return (
+        <div>
+           <h2>This is a Order review pages</h2> 
+        </div>
+    );
+};
+
+export default OrdersReview;
